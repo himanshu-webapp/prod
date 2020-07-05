9c4d9d30c09d85f2d8ba72c7b5b3aef9890f173f
Himanshu <himanshu210798@gmail.com>
2020-07-05 12:08:00 +0530

    staticfiles_dir commented

--- CRIndia/settings.py
+++ CRIndia/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles/')
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'staticfilesdir/'),]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR,'staticfilesdir/'),]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
