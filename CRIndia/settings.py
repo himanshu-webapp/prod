c6716d444287144f0cfad06b9cf3078ff6ef0aed
Himanshu <himanshu210798@gmail.com>
2020-07-05 11:28:53 +0530

    Added allowed hosts t historic-congaree-43214 and the localhots. Not running the collect static command yet.

--- CRIndia/settings.py
+++ CRIndia/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','m6-1ewq(d%xm+d=^secv15ik+1s)qln
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG','') != False
-ALLOWED_HOSTS = ['']
+ALLOWED_HOSTS = ['historic-congaree-43214.herokuapp.com','127.0.0.1']
 
 
 # Application definition
